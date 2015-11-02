6fe6662281841182a433a3cc1768117171a7ac68
J. Farid <jrab227@gmail.com>
2015-11-01 20:35:07 -0500

    Added proj9 starter code

--- Proj9/PythagoreanTrips.hs
+++ Proj9/PythagoreanTrips.hs
@@ -1,23 +1,23 @@
-module PythagoreanTrips (
-  isPythagorean
-, Triple
-) where
-
 type Triple = (Int, Int, Int)
-
-generatePairs :: Int -> Int -> [(Int, Int)]
-generatePairs 0 b = [(0, b)]
-generatePairs a b = (a, b):rest
-    where rest = generatePairs (a-1) (b+1)
-
-generateTuples :: Int -> Int -> [Triple]
-generateTuples 0 b = map (\(x, y) -> (0, x, y)) pairs
-    where pairs = generatePairs b 0
-
-generateTuples a b = concat [current, rest]
-    where rest = generateTuples (a-1) (b+1)
-          current = map (\(x, y) -> (a, x, y)) pairs
-          pairs = generatePairs b 0
 
 isPythagorean :: Triple -> Bool
 isPythagorean (a, b, c) = (a < b) && (b < c) && ( ((a^(2 :: Integer)) + (b^(2 :: Integer))) == (c^(2 :: Integer)) )
+
+fan :: Triple -> [Triple]
+fan (x,y,z) = [(x + 1, y, z), (x, y + 1, z), (x, y, z + 1)]
+
+convolve :: Int -> [Triple]
+convolve 1 = [(0, 0, 1), (0, 1, 0), (1, 0, 0)]
+convolve x = do
+	prev <- convolve (x - 1)
+	updated <- fan prev
+	return updated
+
+filt :: [Triple] -> IO [Triple]
+filt y = return (filter (\x -> isPythagorean x) (y))
+
+main :: IO [Triple]
+main = do
+	x <- (filt (convolve 1000))
+	putStrLn (show x)
+	return x
